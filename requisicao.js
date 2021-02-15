ac847a96b368c4d2c476f0e057dd4e054f18be9e
JosiasLopes <jobercardinho2014@gmail.com>
2021-02-15 12:45:57 -0300

    Estrutura html completa, requisição a api trazendo os dados nome da criptomoeda, simbolo, data historica, além de um icone generico para as criptomoedas

--- requisicao.js
+++ requisicao.js
@@ -1,4 +1,4 @@
-
+var texto = '';
 var url="https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=";
 var apikey = {key:"sua chave aqui"};
 fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY='+apikey.key).then((response)=>{
@@ -6,14 +6,25 @@ fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY='
 		return response.json();
 }).then((api)=>{
 	
-	var texto = "";
+	
 	for(let i=0;i<10;i++){
 		
-		texto = texto+'
-			<div class="media">
-				<img src="coin.jpg" class="align-self-center"
-			</div>
-		';
+		
+		var date = new Date(api.data[i].first_historical_data);
+var day = date.getDate();
+var month = date.getMonth();
+var year = date.getFullYear();
+var dateFormatted = day +'/'+ (month++) +'/'+ year;
+		
+		
+		document.getElementById("coins").innerHTML = texto;
+		texto = texto+"<div class='media'><img src='coin_icon.png' class='align-self-center' witdh='100' height='50'"+
+			"<br>"+"</div><div class='media-body'>"+"<h5 class='mt-2'>"+api.data[i].name+"</h5>"+
+				"<p>"+api.data[i].name+"</p>"+
+				"<p>"+" 1° data historica"+dateFormatted.toString()+"</p>"+
+			"</div>";
 	}
-	console.log(api)}
+	document.getElementById("coins").innerHTML = texto;}
 	).catch((erro)=>console.error(erro.message));
+	
+
